ee1a328e5658d0f9e266e01460e8cdd0e15fe5c5
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2022-03-30 14:29:55 -0400

    add assert and assert eq (not working)

--- asphr/assert.hpp
+++ asphr/assert.hpp
@@ -1,100 +1,7 @@
-//
-// Copyright 2022 Anysphere, Inc.
-// SPDX-License-Identifier: GPL-3.0-only
-//
 
 #pragma once
 
-#include <stdlib.h>
-
-#include <ctime>
-#include <filesystem>
-#include <fstream>
 #include <iostream>
-#include <mutex>
-#include <string>
-#include <thread>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
-#include <vector>
-
-#include "absl/hash/hash.h"
-#include "absl/random/random.h"
-#include "absl/status/status.h"
-#include "absl/status/statusor.h"
-#include "absl/strings/escaping.h"
-#include "absl/strings/numbers.h"
-#include "absl/strings/str_cat.h"
-#include "absl/strings/str_format.h"
-#include "absl/strings/str_split.h"
-#include "absl/time/clock.h"
-#include "absl/time/time.h"
-#include "third_party/json/nlohmann_json.h"
-#include "utils.hpp"
-
-using std::array;
-using std::cerr;
-using std::cout;
-using std::endl;
-using std::lock_guard;
-using std::make_pair;
-using std::make_shared;
-using std::make_tuple;
-using std::make_unique;
-using std::pair;
-using std::shared_ptr;
-using std::string;
-using std::to_string;
-using std::tuple;
-using std::unique_ptr;
-using std::vector;
-
-namespace asphr {
-using json = nlohmann::json;
-
-using absl::Base64Escape;
-using absl::Base64Unescape;
-using absl::InvalidArgumentError;
-using absl::Status;
-using absl::StatusOr;
-using absl::StrCat;
-using absl::StrFormat;
-using absl::Time;
-
-using std::unordered_map;
-using std::unordered_set;
-}  // namespace asphr
-
-#define CEIL_DIV(a, b) (((a) + (b)-1) / (b))
-
-// MESSAGE_SIZE is the size of the message in bytes
-constexpr size_t MESSAGE_SIZE = 1024;
-constexpr size_t MESSAGE_SIZE_BITS = MESSAGE_SIZE * 8;
-
-// pir_value is the type of the value stored at one index in the database
-using pir_value_t = array<byte, MESSAGE_SIZE>;
-// pir_index is the type of the index in the database. this should correspond to
-// the index in the schema/messenger.proto definition
-using pir_index_t = uint32_t;
-
-// initiate the debug mode macros
-#ifdef DEBUG
-#define dbg(x) x
-// x is the value, y is the message
-#define debug(x, y) cout << y << ": " << x << endl
-#define DEBUG_PRINT(x) std::cout << x << std::endl
-#define DEBUG_PRINT_VAR(x) std::cout << #x << ": " << x << std::endl
-#define DEBUG_PRINT_VAR_VAR(x, y) \
-  std::cout << #x << ": " << x << " " << #y << ": " << y << std::endl
-#else
-#define dbg(x)
-#define debug(x)
-#define DEBUG_PRINT(x)
-#define DEBUG_PRINT_VAR(x)
-#define DEBUG_PRINT_VAR_VAR(x, y)
-#endif
 
 // from:
 // https://cs.github.com/abseil/abseil-cpp/blob/3204cc0625230e9876f0310a6dea0014210ab325/absl/base/config.h#L171
@@ -147,7 +54,7 @@ using pir_index_t = uint32_t;
 #define ASPHR_PREDICT_TRUE(x) (x)
 #endif
 
-// copied from BOOST:
+// the following is copied from BOOST. license:
 // Boost Software License - Version 1.0 - August 17th, 2003
 
 // Permission is hereby granted, free of charge, to any person or organization
@@ -172,24 +79,28 @@ using pir_index_t = uint32_t;
 // ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
-inline void assertion_failed_msg(char const* expr, char const* msg,
-                                 char const* function, char const* /*file*/,
-                                 long /*line*/) {
-  std::cerr << "Expression '" << expr << "' is false in function '" << function
-            << "': " << (msg ? msg : "<...>") << ".\n"
+inline void assertion_failed_msg(char const* expr, char const* msg) {
+  std::cerr << "Expression '" << expr << "' is false in function '" << __func__
+            << "': " << (msg ? msg : "<no msg>") << ".\n"
             << "Backtrace:\n"
             << boost::stacktrace::stacktrace() << '\n';
-  /*<-*/ std::exit(0); /*->*/
-                       /*=std::abort();*/
+  std::abort();
 }
 
-inline void assertion_failed(char const* expr, char const* function,
-                             char const* file, long line) {
-  ::boost::assertion_failed_msg(expr, 0 /*nullptr*/, function, file, line);
+inline void assertion_failed(char const* expr) {
+  assertion_failed_msg(expr, nullptr);
+}
+
+inline void assertion_failed_eq(char const* a, char const* b) {
+  assertion_failed_msg(expr, nullptr);
 }
 
 // asserts
 #ifdef DEBUG
-#define ASPHR_ASSERT(expr)                          \
-  (ABSL_PREDICT_TRUE((expr)) ? static_cast<void>(0) \
-                             : [] { assert(false && #expr); }())
+#define ASPHR_ASSERT(expr) \
+  (ABSL_PREDICT_TRUE((expr)) ? static_cast<void>(0) : assertion_failed(#expr))
+#define ASPHR_ASSERT_EQ(a, b)                         \
+  (ABSL_PREDICT_TRUE((a == b)) ? static_cast<void>(0) \
+                               : assertion_failed_eq(#a, #b))
+#else
+#endif
